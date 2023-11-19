b2e54e9cc44caf4291864951ce2e7515d3a30347
emannuel <emannuelloxt@hotmail.com>
2023-11-18 20:01:36 -0400

    fix: encrypt password before search on db

--- services/users.go
+++ services/users.go
@@ -100,7 +100,7 @@ func (s *usersService) Search(status string) (users.Users, *errors.RestErr) {
 func (s *usersService) LoginUser(request users.LoginRequest) (*users.User, *errors.RestErr) {
 	dao := &users.User{
 		Email:    request.Email,
-		Password: request.Password,
+		Password: crypto_utils.GetMd5(request.Password),
 	}
 
 	if err := dao.FindByEmailAndPassword(); err != nil {
